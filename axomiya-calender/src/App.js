1196c613213b6dad1baf214903888173272be6d3
Rituraj Deka <dekarituraj95@gmail.com>
2024-01-20 23:45:29 +0530

    20jan

--- axomiya-calender/src/App.js
+++ axomiya-calender/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 //import Calender1 from './component/Calender1'
-import Xaon from './component/Xaon'
+import Navbar from './component/Navbar'
+import Bohag from './component/Bohag'
+
 
 import './component/Calender.css'
 
@@ -8,10 +10,11 @@ import './component/Calender.css'
 const App = () => {
   return (
     <div>
+      <Navbar></Navbar>
+      <Bohag></Bohag>
     
 
-      <Xaon></Xaon>
-      
+  
       
       
    
